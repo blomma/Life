75feec124412e4074d9f056cee7196fe94013a5b
Mikael Hultgren <blomma@gmail.com>
2016-06-19 16:42:44 +0200

    Comments away

--- Operators.swift
+++ Operators.swift
@@ -1,11 +1,3 @@
-//
-//  Operators.swift
-//  gameoflife
-//
-//  Created by Mikael Hultgren on 19/06/16.
-//  Copyright © 2016 Mikael Hultgren. All rights reserved.
-//
-
 infix operator !~= { associativity left precedence 130 }
 func !~= <Bound>(pattern: CountableClosedRange<Bound>, value: Bound) -> Bool {
 	return !(pattern ~= value)
